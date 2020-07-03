09ad86ccb6497d33c442df0abb5db71e41de38f6
unknown <fe97964@gmail.com>
2020-07-03 09:56:03 -0300

    IAgenda Inicial

---
+++ IAgenda.cs
@@ -0,0 +1,9 @@
+namespace Aula31WhatsApp
+{
+    public interface IAgenda
+    {
+        void Cadastrar();
+        void Excluir();
+        void Listar();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula31WhatsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
